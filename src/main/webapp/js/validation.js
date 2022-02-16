b7ab7db3e5524a6deef2c139576b595703ba11d6
pantellerija <polina.pavlova19.10@gmail.com>
2022-02-16 18:22:56 +0300

    suffer

--- src/main/webapp/js/validation.js
+++ src/main/webapp/js/validation.js
@@ -1,62 +1,64 @@
 $(document).ready(function () {
-    let canvas = $('#canvas');
+    let x, y, radius,
+        alert = $("#message .error"),
+        message,
+        colour = "red", //default
+        canvas = $("#canvas"),
+        result_style = document.getElementById('row').style;
 
-    const COEFF = 67;
-    const AXIS = 110;
-
-    const Y_MIN = -3;
-    const Y_MAX = 3;
-    const X_VALUES = ['-2', '-1.5', '-1', '-0.5', '0', '0.5', '1', '1.5', '2'];
-    const y_error = document.querySelector('#y_val+span.error');
-
-    let x;
-    let radius;
-    let message;
-    let isValid = false;
-    let yField = document.getElementById("y_val");
-
-    let value = $("#y_val").val().replace(',', '.');
-    let result_style = document.getElementById('row').style;
+    const K = 68,
+        AXIS = 110,
+        Y_MIN = -3,
+        Y_MAX = 3,
+        X_VALUES = ['-2', '-1.5', '-1', '-0.5', '0', '0.5', '1', '1.5', '2'],
+        R_VALUES = ['1', '2', '3', '4', '5'];
 
 
-    function getRadius() {
-        $('.set_r').on('click', function () {
-            radius = $(this).val();
-        })
-    }
-
-
-    function getX() {
-        $('input[name="x_value"]').on('check', function () {
-            x = $('input[name="x_value"]:checked').val();
-        })
+    function isCorrectInput(number) {
+        return !isNaN(number);
     }
 
     function validateX() {
-        if ($('input[name="x_value"]').not(':checked')) {
-            isValid = false;
+        x = $('input[name="x_value"]:checked').val();
+        if (isCorrectInput(x) && X_VALUES.includes(x)) {
+            alert.text("");
+            alert.removeClass("active");
+            return true;
+        } else {
+            alert.text("Введите значение X!");
+            alert.addClass("active");
+            return false;
         }
     }
 
-
-    function isValidValue(node) {
-        return node.validity.valid;
-    }
-
-
     function validateY() {
-        yField.addEventListener("input", function (event) {
-            if (isValidValue(yField)) {
-                isValid = true;
-                y_error.textContent = '';
-                y_error.className = 'error';
-            } else {
-                showMessage();
-                event.preventDefault();
-            }
-        });
+        y = $("#y_val").val().replace(',', '.');
+        if (isCorrectInput(y) && y <= Y_MAX && y >= Y_MIN) {
+            alert.text("");
+            alert.removeClass("active");
+            return true;
+        } else {
+            alert.text("Введите значение Y в диапазоне от " + Y_MIN + " до " + Y_MAX);
+            alert.addClass("active");
+            return false;
+        }
     }
 
+    function validateR() {
+        if (isCorrectInput(radius) && R_VALUES.includes(radius)) {
+            alert.text("");
+            alert.removeClass("active");
+            return true;
+        } else {
+            alert.text("Введите значение R!");
+            alert.addClass("active");
+            return false;
+        }
+    }
+
+    function validateAllInputs() {
+        return validateX() && validateY() && validateR();
+    }
 
     function checkFirstQuarter(x, y, r) {
         return x <= 0 && y >= 0 && x * x + y * y <= r / 2;
@@ -70,26 +72,8 @@ $(document).ready(function () {
         return x >= 0 && y <= 0 && y >= x - r / 2;
     }
 
-
     function checkCoordinates(x, y, r) {
         return checkFirstQuarter(x, y, r) || checkSecondQuarter(x, y, r) || checkForthQuarter(x, y, r);
-    }
-
-
-    function showMessage() {
-        if (yField.validity.valueMissing || !(value instanceof Number)) {
-            y_error.textContent = 'Пожалуйста, введите число! :(';
-        } else if (!isNaN(parseFloat(value))) {
-            y_error.textContent = 'Вы ввели невалидное число!';
-        }
-
-        if (yField.validity.rangeOverflow) {
-            y_error.textContent = 'Максимальное значение y = 3!';
-        }
-        if (yField.validity.rangeUnderflow) {
-            y_error.textContent = "Минимальное значение у = -3!"
-        }
-        y_error.className = 'error active';
     }
 
     function clearCanvas() {
@@ -98,32 +82,72 @@ $(document).ready(function () {
 
     function drawPoint(x, y, color) {
         clearCanvas();
-        let Axes = canvas[0].getContext('2d');
+        let axes = canvas[0].getContext('2d');
         if (x > canvas.width() || x < -canvas.width() || y > canvas.height() || y < -canvas.height()) {
             return;
         }
-        Axes.setLineDash([2, 2]);
-        Axes.beginPath();
-        Axes.moveTo(x, 110);
-        Axes.lineTo(x, y);
-        Axes.moveTo(110, y);
-        Axes.lineTo(x, y);
-        Axes.stroke();
-        Axes.fillStyle = color;
-        Axes.arc(x, y, 2, 0, 2 * Math.PI);
-        Axes.fill();
+        axes.setLineDash([2, 2]);
+        axes.beginPath();
+        axes.moveTo(x, 110);
+        axes.lineTo(x, y);
+        axes.moveTo(110, y);
+        axes.lineTo(x, y);
+        axes.stroke();
+        axes.fillStyle = color;
+        axes.arc(x, y, 2, 0, 2 * Math.PI);
+        axes.fill();
+    }
+
+    function drawRowPoint(x, y, r, colour) {
+        let ctxPoints = canvas[0].getContext('2d');
+        ctxPoints.fillStyle = colour;
+        ctxPoints.beginPath();
+        ctxPoints.arc(x / r * K + AXIS, -y / r * K + AXIS, 2, 0, 2 * Math.PI);
+        ctxPoints.fill();
+    }
+
+    function redrawAllPointers() {
+        $("#row tr").each(function (index) {
+            let data = $(this).find('td');
+            let new_x = data.eq(0).text().trim();
+            let new_y = data.eq(1).text().trim();
+            console.log(index + " row-coordinates: x ( " + new_x + " ), y ( " + new_y + " ), r ( " + radius + " )");
+            colour = checkCoordinates(new_x, new_y, radius) ? "green" : "red";
+            drawRowPoint(new_x, new_y, radius, colour);
+        })
     }
 
     function redrawFromInput(x, y, radius) {
-        let color = 'red';
-        if (checkCoordinates(x, y, radius)) {
-            color = 'green';
+        if (!validateAllInputs()) {
+            clearCanvas();
+            return;
         }
-        drawPoint(x * COEFF / radius + AXIS, -(y / radius * COEFF - AXIS), color);
+        colour = checkCoordinates(x, y, radius) ? "green" : "red";
+        drawPoint(x * K / radius + AXIS, -(y / radius * K - AXIS), colour);
     }
 
-    function clickDraw(event) {
-        let x = (event.offsetX - AXIS) / COEFF * radius;
+    $(".set_r").on("click", function () {
+        radius = $(this).val();
+        let svgGraph = document.querySelector(".result-graph").getSVGDocument();
+        svgGraph.querySelector('.coordinate-text_minus-Rx').textContent = (-radius).toString();
+        svgGraph.querySelector('.coordinate-text_minus-Ry').textContent = (-radius).toString();
+        svgGraph.querySelector('.coordinate-text_minus-half-Rx').textContent = (-radius / 2).toString();
+        svgGraph.querySelector('.coordinate-text_minus-half-Ry').textContent = (-radius / 2).toString();
+        svgGraph.querySelector('.coordinate-text_plus-Rx').textContent = (radius).toString();
+        svgGraph.querySelector('.coordinate-text_plus-Ry').textContent = (radius).toString();
+        svgGraph.querySelector('.coordinate-text_plus-half-Rx').textContent = (radius / 2).toString();
+        svgGraph.querySelector('.coordinate-text_plus-half-Ry').textContent = (radius / 2).toString();
+        redrawFromInput(x, y, radius);
+    });
+
+    $('#y_val').on('change', () => redrawFromInput(x, y, radius));
+    $('.set_X').on('check', () => redrawFromInput(x, y, radius));
+
+    canvas.on('click', function (event) {
+        if (!validateR()) {
+            return;
+        }
+        let x = (event.offsetX - AXIS) / K * radius;
         let minDiff = Infinity;
         let nearestXValue;
         for (let i = 0; i < X_VALUES.length; i++) {
@@ -133,30 +157,30 @@ $(document).ready(function () {
             }
         }
 
-        let y = (-event.offsetY + AXIS) / COEFF * radius;
-        if (y < Y_MIN) y = Y_MIN;
-        if (y > Y_MAX) y = Y_MAX;
+        let y_value = (-event.offsetY + AXIS) / K * radius;
+        if (y_value < Y_MIN) y_value = Y_MIN;
+        if (y_value > Y_MAX) y_value = Y_MAX;
 
-        let color = 'red';
-        if (checkCoordinates(x, y, radius)) {
-            color = 'green';
-        }
+        colour = checkCoordinates(nearestXValue, y_value, radius) ? "green" : "red";
 
-        drawPoint(COEFF * nearestXValue / radius + AXIS, -(y / radius * COEFF - AXIS), color);
-    }
+        let new_x = $('input[name="x_value"][value="' + nearestXValue.trim() + '"]');
+        new_x.trigger("click");
 
-    canvas.on('click', clickDraw);
+        $("#y_val").val(y_value);
+        $("#subbtn").trigger('click');
 
+        drawPoint(K * nearestXValue / radius + AXIS, -(y / radius * K - AXIS), colour);
+    });
 
-
-    getX();
-    validateX();
-    validateY();
     $("#form").on("submit", function (event) {
         event.preventDefault();
 
-        if (!isValid) {
+        if (!validateAllInputs()) {
+            console.log("fail validation after click!");
             return;
+        } else {
+            // redrawFromInput();
+            console.log("coordinates:" + " x: " + x + " y: " + y + " r: " + radius);
         }
 
         $.ajax({
@@ -180,26 +204,6 @@ $(document).ready(function () {
         return true;
     });
 
-
-
-    $(".set_r").on("click", function () {
-        radius = $(this).val();
-        let x = $('input[name="x_value"]:checked').val();
-        let y = $('#y_val').val();
-
-        let svgGraph = document.querySelector(".result-graph").getSVGDocument();
-        svgGraph.querySelector('.coordinate-text_minus-Rx').textContent = (-radius).toString();
-        svgGraph.querySelector('.coordinate-text_minus-Ry').textContent = (-radius).toString();
-        svgGraph.querySelector('.coordinate-text_minus-half-Rx').textContent = (-radius / 2).toString();
-        svgGraph.querySelector('.coordinate-text_minus-half-Ry').textContent = (-radius / 2).toString();
-        svgGraph.querySelector('.coordinate-text_plus-Rx').textContent = (radius).toString();
-        svgGraph.querySelector('.coordinate-text_plus-Ry').textContent = (radius).toString();
-        svgGraph.querySelector('.coordinate-text_plus-half-Rx').textContent = (radius / 2).toString();
-        svgGraph.querySelector('.coordinate-text_plus-half-Ry').textContent = (radius / 2).toString();
-
-        redrawFromInput(x, y, radius);
-    });
-
     $('#form').on("reset", function (event) {
         $.ajax({
             type: 'POST',
@@ -212,16 +216,5 @@ $(document).ready(function () {
         })
     })
 
-
-    $('#y_val').on('change', function () {
-        let y = $('#y_val').val();
-        getRadius();
-        redrawFromInput(x, y, radius);
-    })
-
-    $('.set_X').on('check', function () {
-        let y = $('#y_val').val();
-        redrawFromInput(x, y, radius);
-    })
-
+    setInterval(redrawAllPointers, 600);
 });
